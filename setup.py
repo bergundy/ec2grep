7e15a0c62f0d72be14b3b92da5b97edb74c5127d
Roey Berman <roey.berman@gmail.com>
2017-02-13 10:58:33 +0200

    Revert "Update setup.py"
    This reverts commit 95cace057971c14b5691ebbd830d6b0054ad27ab.

--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='shh',
-    description='Shell helper in python',
+    name='ec2grep',
+    description='EC2 cli tool',
     author='Roey Berman',
     author_email='roey.berman@gmail.com',
-    packages=['shh'],
+    packages=['ec2grep'],
     verion='0.1',
-    keywords=['shell', 'sh'],
+    keywords=['ec2', 'cli', 'aws', 'ssh'],
     install_requires=[
+        'boto3',
+        'futures',
+        'click',
     ],
     entry_points={
+        'console_scripts': [
+            'ec2 = ec2grep:cli'
+        ]
     }
 )
